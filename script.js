94f5dcb9bd64b40f2097a7b263251025cc68baf7
Niamor70 <niamor@zliton.com>
2013-02-14 00:48:32 +0100

    demonstration source

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿function usePosition(pos) {
+    alert("lat:"+pos.coords.latitude+' lng:'+pos.coords.longitude);
+}
+
+function initGeolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(usePosition);
+    } else {
+        alert("HTML5 Geolocation API is not supported by your browser");
+    }
+}
